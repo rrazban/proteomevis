6132419ceecb2ab789d06393cea11f2a255d7449
niamhdurfee <durfee@college.harvard.edu>
2016-11-01 07:46:27 -0400

    Consolidating libraries

--- proteomevis/static/libs/d3click.js
+++
@@ -1,8 +0,0 @@
-jQuery.fn.d3click = function () {
-  this.each(function (i, e) {
-    var evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-
-    e.dispatchEvent(evt);
-  });
-};
